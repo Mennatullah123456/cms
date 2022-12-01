c9f5b199d3546c33867450677983a99190720314
MennaLana <engmenna29@gmail.com>
2022-12-01 23:43:32 +0400

    first commit

---
+++ src/dummyData.js
@@ -0,0 +1,149 @@
+export const userData = [
+    {
+      name: "Jan",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 3000,
+    },
+    {
+      name: "Mar",
+      "Active User": 5000,
+    },
+    {
+      name: "Apr",
+      "Active User": 4000,
+    },
+    {
+      name: "May",
+      "Active User": 3000,
+    },
+    {
+      name: "Jun",
+      "Active User": 2000,
+    },
+    {
+      name: "Jul",
+      "Active User": 4000,
+    },
+    {
+      name: "Agu",
+      "Active User": 3000,
+    },
+    {
+      name: "Sep",
+      "Active User": 4000,
+    },
+    {
+      name: "Oct",
+      "Active User": 1000,
+    },
+    {
+      name: "Nov",
+      "Active User": 4000,
+    },
+    {
+      name: "Dec",
+      "Active User": 3000,
+    },
+  ];
+
+  export const productData = [
+    {
+      name: "Jan",
+      "Sales": 4000,
+    },
+    {
+      name: "Feb",
+      "Sales": 3000,
+    },
+    {
+      name: "Mar",
+      "Sales": 5000,
+    },
+  ];
+
+  export const userRows = [
+    {
+      id: 1,
+      username: "product1",
+      img: "https://mennatullah123456.github.io/E-Commerce-/imgs/home_new-arrival/pr6.jpg",
+      brand: "folu",
+      status: "out-stock",
+      price: 35,
+    },
+    {
+      id: 2,
+      username: "product2",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      brand: "folu",
+      status: "out-stock",
+      price: 42,
+    },
+    {
+      id: 3,
+      username: "product3",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      brand: "folu",
+      status: "out-stock",
+      price: 45,
+    },
+    {
+      id: 4,
+      username: "product0",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      brand: "folu",
+      status: "stock",
+      price: 16,
+    },
+    {
+      id: 5,
+      username: "product4",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      brand: "folu",
+      status: "out of stock",
+      price: 22,
+    },
+    {
+      id: 6,
+      username: "product5",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      brand: "folu",
+      status: "stock",
+      price: 15,
+    },
+    {
+      id: 7,
+      productname: "product7",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      brand: "folu",
+      status: "passive",
+      price: 44,
+    },
+    {
+      id: 8,
+      productname: "product8",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      brand: "folu",
+      status: "stock",
+      price: 36,
+    },
+    {
+      id: 9,
+      productname: "product9",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      brand: "folu",
+      status: "in-stock",
+      price: 65,
+    },
+    {
+      id: 10,
+      productname: "product10",
+      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      email: "snow@gmail.com",
+      status: "stock",
+      price: 65,
+    },
+  
+  ];
